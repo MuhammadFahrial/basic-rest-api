f2965cf1384012e2fff2efb8d0c78ad2e5078acc
MuhammadFahrial <rialjhe111@gmail.com>
2024-09-19 11:38:11 +0800

    update:routes

--- routes/UsersRoute.js
+++ routes/UsersRoute.js
@@ -3,6 +3,7 @@ import {
   createUsers,
   deleteUsers,
   getUsers,
+  getUsersById,
   updateUsers,
 } from "../controllers/UsersControllers.js";
 import { usersMiddleware } from "../middleware/UsersMiddleWare.js";
@@ -10,6 +11,7 @@ import { usersMiddleware } from "../middleware/UsersMiddleWare.js";
 const router = express.Router();
 
 router.get("/users", usersMiddleware, getUsers);
+router.get("/users/:id", getUsersById);
 router.post("/users", createUsers);
 router.patch("/users/:id", updateUsers);
 router.delete("/users/:id", deleteUsers);
